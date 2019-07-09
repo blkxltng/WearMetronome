30ade2aaf7f400e00acc12f7a5232eb61c056acb
Max <firejama@gmail.com>
2019-07-09 15:58:52 -0400

    added simple count

--- app/src/main/java/com/blkxltng/wearmetronome/MainActivity.java
+++ app/src/main/java/com/blkxltng/wearmetronome/MainActivity.java
@@ -4,16 +4,38 @@ import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
+
+    int testNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        mTextView = findViewById(R.id.text);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        testNum++;
+                        if(testNum > 4) {
+                            testNum = 1;
+                        }
+                        mTextView.setText((Integer.toString(testNum)));
+                    }
+                });
+            }
+        }, 0, 1000);
 
         // Enables Always-on
         setAmbientEnabled();
